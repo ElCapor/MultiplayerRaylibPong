6e535afa985ce3ed03e2b70d456963521e63cc03
ElCapor <104172250+ElCapor@users.noreply.github.com>
2023-11-23 21:44:36 +0100

    Working paddle

---
+++ PongRaylib/Player.cs
@@ -0,0 +1,72 @@
+﻿using Raylib_cs;
+using System.Numerics;
+using static Raylib_cs.Raylib;
+
+class Player
+{
+    int id = -1;
+
+    Vector2 position;
+    Vector2 velocity;
+    Vector2 size;
+
+    public EventHandler<Player>? onPlayerMoveDown;
+    public EventHandler<Player>? onPlayerMoveUp;
+    public EventHandler<Player>? onPlayerStopMove;
+
+    public Player()
+    {
+        position = new();
+        size = new(10, 50);
+        velocity = new();
+    }
+
+    public void StartMoveUp()
+    {
+        if (position.Y + size.Y > 0)
+        {
+            velocity.X = 0;
+            velocity.Y = -5;
+            onPlayerMoveUp?.Invoke(this, this);
+        }
+        else
+        {
+            StopMove();
+            position.X = 0;
+            position.Y = 0 + size.Y;
+        }
+    }
+
+    public void StartMoveDown()
+    {
+        if (position.Y + size.Y < 800)
+        {
+            velocity.X = 0;
+            velocity.Y = 5;
+            onPlayerMoveDown?.Invoke(this, this);
+        }
+        else
+        {
+            StopMove();
+            position.X = 0;
+            position.Y = 800 - size.Y;
+        }
+    }
+
+    public void StopMove()
+    {
+        velocity.Y = 0;
+        velocity.X = 0;
+        onPlayerStopMove?.Invoke(this, this);
+    }
+
+    public void Update()
+    {
+        position += velocity;
+    }
+
+    public void Draw()
+    {
+        DrawRectangle((int)position.X, (int)position.Y, (int)size.X, (int)size.Y, Color.RED);
+    }
+}
--- PongRaylib/Program.cs
+++ PongRaylib/Program.cs
@@ -1,18 +1,36 @@
-﻿using Raylib_cs;
-using static Raylib_cs.Raylib;
+﻿using static Raylib_cs.Raylib;
+using static Raylib_cs.KeyboardKey;
+using Raylib_cs;
 
+static void HandleInput(ref Player p)
+{
+    if (IsKeyPressed(KeyboardKey.KEY_DOWN))
+    {
+        p.StartMoveDown();
+    }
+    else if (IsKeyPressed(KeyboardKey.KEY_UP))
+    {
+        p.StartMoveUp();
+    }
+    else if (IsKeyReleased(KeyboardKey.KEY_UP) || IsKeyReleased(KeyboardKey.KEY_DOWN))
+    {
+        p.StopMove();
+    }
+}
 
-
-
-
-
+Player p = new();
 
 InitWindow(800, 600, "Pong");
 SetTargetFPS(60);
 
 while (!WindowShouldClose())
 {
-
+    p.Update();
+    HandleInput(ref p);
+    BeginDrawing();
+    ClearBackground(Color.BLACK);
+    p.Draw();
+    EndDrawing();
 }
 
 CloseWindow();
